70c431e01f5a3b96bdcb42949ca4cf22762f4494
krismanaya <krismanaya@gmail.com>
2016-07-30 11:29:52 -0700

    Fix some text

--- grades.py
+++ grades.py
@@ -1,23 +1,23 @@
 grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
 print "Grades:", grades
 
-#prints each number in the sequence of grades. 
+#Print each number in the sequence of grades. 
 def print_grades(grades): 
     for num in grades: 
         print num
 
-#returns the total of graded exams. 
+#Return the total of graded exams. 
 def grades_sum(scores):
     total = 0 
     for grades in scores: 
         total += grades
     return total 
 
-#return the average of the total graded exams. 
+#Return the average of the total graded exams. 
 def grades_average(grades):
     return grades_sum(grades) / float(len(grades))
 
-#computes the variance. 
+#Compute the variance. 
 def grades_variance(scores): 
     average = grades_average(scores)
     variance = 0 
@@ -27,6 +27,7 @@ def grades_variance(scores):
         new_variance = variance / float(len(scores))
     return new_variance
 
+#Compute the standard deviation. 
 def grades_std_deviation(variance): 
     return variance**0.5
 
