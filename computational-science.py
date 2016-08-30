171762654486fd5768083da9bc0835a98d4235e5
krismanaya <krismanaya@gmail.com>
2016-08-29 19:16:33 -0700

    Create a vector

--- computational-science.py
+++ computational-science.py
@@ -23,3 +23,44 @@ def Bisection(a,b,TOL,N):
             b = p
         i += 1
     return "Method Failed after %d iteration" %(i)
+
+
+class Vector: 
+    """Represent a vector in a multidimensional space."""
+    def __init__(self,d):
+    """Create d-dimensional vector of zerios.""" 
+        self.d = [0]*d 
+
+    def __len__(self)
+    """Return the dimenson of the vector"""
+    return len(self.d)
+
+    def __getitem__(self,j): 
+    """Return jth coordinate of vector."""
+    return self.d[j]
+
+    def __setitem__(self,j,val): 
+        """Set jth coordinage of vector to given value."""
+        self.d[j] = val 
+
+    def __add__(self,other): 
+    """Return sum of two vectors"""
+    if len(self) != len(other): 
+        raise ValueError('dimensions must agree')
+    result = Vector(len(self))
+    for j in range(len(self)): 
+        result[j] = self[j] + other[j]
+    return result 
+
+    def __eg__(self,other): 
+        """Return True if vector has same coordinages as other."""
+        return self.d == other.d
+
+    def __ne__(self,other): 
+        """Return True if vector differs from other"""
+        return not self == other 
+
+    def __str__(self): 
+        """PRoduce string representation of vector."""
+        return "<" + str(self.d)[1:-1] + ">"
+
